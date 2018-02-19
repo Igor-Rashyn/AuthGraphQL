f51842f136cf47d97e4924f8bf1d5960bef3e647
rashyni <igor.rashyn@vizexplorer.com>
2018-02-19 16:58:57 +1300

    Finished login form

--- client/index.js
+++ client/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Route, Router, hashHistory, IndexRoute } from 'react-router';
-import App from './components/App'
+import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -22,7 +23,8 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}> 
+        <Route path="/" component={App}>
+          <Route path="login" component={LoginForm}></Route>
         </Route>
       </Router>
     </ApolloProvider>
